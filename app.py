145a7cc2cfccb090238a7e172b437d4831c28fa2
Dario Cantarelli <dario.cantarelli@mail.polimi.it>
2023-03-06 22:49:54 +0100

    Split Simulator code into another file.

--- app.py
+++ app.py
@@ -1,140 +1,114 @@
+# Web App imports
+
 from flask import Flask, send_from_directory, Response, url_for, jsonify
 from flask_cors import CORS
+
+# Simulator imports
 import math
-import numpy as np
+import scipy.io as sio
 from manim import *
+from model.simulator import Simulator
+
+# GLOBAL VARS
+#  TODO redefine NOT as global variables
+PERIOD = 1 / 44100
+MATRIX = 0.0
+HAMMER = 0.0
 
 
-def shift_wg(val, vector, n, length):
-    if n > length:
-        m = n - np.floor(n/length)*length + 1
-    else:
-        m = n
-
-    out = vector[m]
-    vector[m] = val
-    return out, vector
+# ----- VISUALIZER PART ---------------
 
 
-def nl_felt(val, k):
-    return 1 + 2 * k * val - math.sqrt(1 + 8 * k * val) / (2 * k)
+def plot_string_graph(string_matrix, axes, row_idx):
+    """
+    Plots the piano string graph
+
+    :param string_matrix:
+    :param axes:
+    :param row_idx:
+    :return:
+    """
+
+    print("plotting row: ", int(row_idx))
+    # NOTA: per le colonne si conta x - 1 poichè le coordinate dell'asse sono indicizzate a partire da 1
+    return axes.plot(lambda x: string_matrix[int(row_idx)][int(x) - 1], color=RED)
 
 
-class Simulator:
-    iterations = 88200
-    Fs = 44100
-    Ts = 1 / Fs
-    wgLength = 168
-    wgLengthLeft = math.ceil(wgLength * 0.116)
-    wgLengthRight = wgLength - wgLengthLeft
-    K = 0.98
-    A = 1000
-    str_length = 0.62
-    tension = 670
-    hammer_initial_velocity = 7
-    hs_distance = 0.01
-    Lh = 3.93e-3
-    Z = 10.28
+def get_hammer(hammer_matrix, axes, position_idx):
+    """
+    Draws the piano hammer
 
-    # Adaptations conditions
-    Z1 = Z
-    Z2 = Z
-    Z3 = Z1 + Z2
-    gammaSer = Z1 / Z3
-    Z4 = Z3
-    Z5 = 2 * Lh / Ts
-    Z6 = Z4 * Z5 / (Z4 + Z5)
-    gammaPar = Z6 / Z4
-    C = Ts / (2 * Z6)
-    Z7 = Z6
+    :param hammer_matrix:
+    :param axes:
+    :param position_idx:
+    :return:
+    """
+    # center_y_coord = hammer_matrix[0][int(position_idx) - 1]
+    center_y_coord = hammer_matrix[int(position_idx) - 1]  # TODO comment when hammer is 2D matrix
+    center_point = Dot(axes.c2p(19, center_y_coord, 0))
+    circle = Circle(color=BLUE, fill_color=BLUE, fill_opacity=1.0).surround(center_point, buffer_factor=2.0)
+    circle.move_to(circle.get_bottom())
+    return circle
 
-    felt_slope = (A * C - 1) / (A * C + 1)
-    Knl = C * A
 
-    wgLeft = np.zeros(wgLengthLeft)
-    wgRight = np.zeros(wgLengthRight)
-    string = np.zeros(iterations)
-    hammer = np.zeros(iterations)
+class Visualizer(Scene):
+    """
+    Class used to visualize Piano Hammer-String Interaction
+    """
+    #  TODO find if you can pass string-hammer matrices directly to Scene or you need to encapsulate it in superclass
+    def construct(self):
+        # Config settings
+        # config.custom_folders = True
+        # config.media_dir = './assets/esticazzi'
+        # config.output_file = 'prova-video.mp4'
+        # config.renderer = "opengl"
+        # config.write_to_movie = True
+        config.flush_cache = True
+        config.disable_caching = True
 
-    a1 = 0
-    a2 = 0
-    b3 = -a1 - a2
-    a4 = b3
-    a5 = Z5 * hammer_initial_velocity / 2
-    b6 = a5 - gammaPar * (a5 - a4)
-    a7 = b6
-    b8 = - hs_distance / (2 * C)
-    wave_integrator_delay_old = b8 - a7
-    a8 = nl_felt(b8, felt_slope)
-    wave_integrator_delay = a7 - a8
-    b7 = a8 + wave_integrator_delay_old
-    a6 = b7
-    b5 = a6 - gammaPar * (a5 - a4)
-    b4 = -a4 + a5 + b5
-    a3 = b4
-    b1 = a1 - gammaSer * (a1 + a2 + a3)
-    b2 = -a3 - b1
+        # -------------- MATLAB DATA ------------------------------------- #
+        # string = sio.loadmat('wg_out.mat')['string_matrix'] # MATLAB string
+        # string = sio.loadmat('wg_out_correct_wg_shift.mat')['string_matrix'] # MATLAB string
+        # hammer_positions = sio.loadmat('hammer.mat')['hammer']  # MATLAB hammer
 
-    hammer_velocity = (a5 - b5) / Z5
-    hammer_position = (b8 - a8) * C
-    hammer_initial_position = hammer_position
-    string_velocity = (a1 - b1) / Z1
-    string_position = 0
+        # -------------- PYTHON DATA --------------------------------------#
+        string = MATRIX  # string obtained from current simulation
+        hammer_positions = HAMMER  # PYTHON hammer
 
-    string_matrix = np.zeros((iterations, int(wgLength / 2)))
-    # init other values
-    string_velocity_old = 0
-    hammer_velocity_old = 0
-    hammer_position_old = 0
-    string_position_old = 0
+        string_shape = string.shape
+        string_max_value = np.max(string)
+        # print('string max: ', string_max_value)
+        # define axes NB y range needs to be specified in that way
+        axes = Axes(
+            x_range=[0, string_shape[1], 1],
+            y_range=[-int(string_max_value), int(string_max_value), 1],
+            # y_range=[-5, 5, 0.5],
+            x_length=10,
+            y_length=10,
+            tips=False
+        )
+        # axes.add_coordinates()
+        axes_labels = axes.get_axis_labels()
+        idx_tracker = ValueTracker(0)
+        string_graph = always_redraw(lambda: plot_string_graph(string, axes, idx_tracker.get_value()))
 
-    def run_simulation(self):
-        for n in range(1, self.iterations):
-            wg_left_new_values = shift_wg(-self.b1, self.wgLeft, n, self.wgLengthLeft)
-            self.a1 = wg_left_new_values[0]
-            self.wgLeft = wg_left_new_values[1]
-            wg_right_new_values = shift_wg(self.K * self.b2, self.wgRight, n, self.wgLengthRight)
-            self.a2 = wg_right_new_values[0]
-            self.wgRight = wg_right_new_values[1]
+        # string_label = axes.get_graph_label(string_graph, label='PIANO STRING')
+        # plot = VGroup(axes, string_graph)
+        # plot = VGroup(string_graph, axes_labels)
+        # labels = VGroup(axes_labels)
+        hammer_center_point = Dot(point=axes.c2p(19, 0, 0), color=YELLOW)
+        hammer = always_redraw(lambda: get_hammer(hammer_positions, axes, idx_tracker.get_value()))
 
-            self.wave_integrator_delay_old = self.wave_integrator_delay
-            self.string_velocity_old = self.string_velocity
-            self.string_position_old = self.string_position
-            self.hammer_velocity_old = self.hammer_velocity
-            self.hammer_position_old = self.hammer_position
+        self.add(string_graph, hammer)
 
-            self.b3 = -self.a1 - self.a2
-            self.a4 = self.b3
-            self.a5 = -self.b5
-            self.b6 = self.a5 - self.gammaPar*(self.a5 - self.a4)
-            self.a7 = self.b6
-            self.b8 = self.a7 + self.wave_integrator_delay_old
-            self.a8 = nl_felt(self.b8, self.Knl)
-            self.wave_integrator_delay = self.a7 - self.a8
-            self.b7 = self.a8 + self.wave_integrator_delay_old
-            self.a6 = self.b7
-            self.b5 = self.a6 - self.gammaPar*(self.a5 - self.a4)
-            self.b4 = -self.a4 + self.a5 + self.b5
-            self.a3 = self.b4
-            self.b1 = self.a1 - self.gammaSer*(self.a1 + self.a2 + self.a3)
-            self.b2 = -self.a3 - self.b1
+        # add sound
+        self.add_sound("string.wav", time_offset=1, gain=1)  # regolare time_offset in base al numero di wait effettuate prima che parta il video
+        self.wait()
+        # TODO check se la duration dell'animazione deve essere di string_shape[0] o string_shape[0] - 1
+        self.play(ApplyMethod(idx_tracker.increment_value, (string_shape[0] - 1)), run_time=PERIOD * (string_shape[0] - 1))
+        self.wait()
 
-            self.string_velocity = (self.a1 - self.b1)/self.Z1
-            self.string_position = self.string_position_old + (self.string_velocity + self.string_velocity_old)*self.Ts/2
-            self.string[n] = self.string_position
-            self.hammer_velocity = (self.a5 - self.b5)/self.Z5
-            self.hammer_position = self.hammer_position_old + (self.hammer_velocity + self.hammer_velocity_old)* self.Ts / 2
-            self.hammer[n] = self.hammer_position
-            # fill string_matrix TODO verify array indexing is correct
-            x = np.array(1, self.wgLength/2)
-            l1 = self.wgLeft[0:self.wgLengthLeft/2 - 1]
-            l2 = self.wgLeft[self.wgLengthLeft/2:self.wgLengthLeft - 1]
-            left = np.flip(l2) + l1
-            r1 = self.wgRight[1:self.wgLengthRight/2 - 1]
-            r2 = self.wgRight[self.wgLengthRight/2: self.wgLengthRight-1]
-            right = np.flip(r2) + r1
-            self.string_matrix[n][:] = np.concatenate(left, right)
-            return self.string_matrix
+# --- WEB APP PART ---
 
 
 app = Flask(__name__)
@@ -156,7 +130,7 @@ def get_wdf_video():
 
 
 if __name__ == '__main__':
-    # simulator = Simulator()
+    simulator = Simulator()
     # matrix = simulator.run_simulation()
     # print('Obtained matrix from simulation is: ', matrix)
-    app.run() #TODO uncomment to run web application
+    app.run()
---
+++ model/simulator.py
@@ -0,0 +1,194 @@
+def dwg_shift(val, wg, n):
+    """
+    Updates the digital waveguide
+
+    :param val: Incoming value
+    :param wg: Waveguide to update
+    :param n: index of the current step
+    :return: last updated value of the waveguide
+    """
+    # m = np.remainder(n, wg.size)
+    m = n % wg.size
+    out = wg[m]
+    wg[m] = val
+    return out
+
+
+def nl_felt(val, k):
+    """
+    Models the response of a nonlinear piano hammer felt
+
+    :param val:
+    :param k:
+    :return:
+    """
+    if val < 0:
+        out = -val
+    else:
+        out = (1 + 2 * k * val - math.sqrt(1 + 8 * k * val)) / (2 * k)
+    return out
+
+
+class Simulator:
+    """
+    Class modeling the piano hammer-string interaction
+    """
+    def __init__(self):
+        self.iterations = np.uintc(88200)
+        self.Fs = np.uintc(44100)
+        self.Ts = np.double(1 / self.Fs)
+        self.wg_length = np.uintc(168)
+        self.wg_length_left = np.uintc(math.ceil(self.wg_length * 0.116))
+        # print('wg_length_left: ', self.wg_length_left)
+        self.wg_length_right = np.uintc(self.wg_length - self.wg_length_left)
+        # print('wg_length_right: ', self.wg_length_right)
+        self.K = np.double(0.98)  # soundboard reflection coefficient
+        self.A = np.uintc(1000)  # linear felt stiffness TODO check if better int or float
+        self.str_length = np.double(0.62)  # string length
+        self.tension = np.double(670)
+        self.hammer_initial_velocity = np.double(7)
+        self.hs_distance = np.double(0.01)  # hammer-string distance
+        self.Lh = np.double(3.93e-3)
+        self.Z = np.double(10.28)  # characteristic impedance of the string
+
+        # Adaptations conditions (connection of WDF blocks)
+        self.Z1 = self.Z
+        self.Z2 = self.Z
+        self.Z3 = self.Z1 + self.Z2
+        self.gamma_ser = self.Z1 / self.Z3
+        self.Z4 = self.Z3
+        self.Z5 = 2 * self.Lh / self.Ts
+        self.Z6 = self.Z4 * self.Z5 / (self.Z4 + self.Z5)
+        self.gamma_par = self.Z6 / self.Z4
+        self.C = self.Ts / (2 * self.Z6)  # Ref. capacitance of the mutator for the NL spring
+        self.Z7 = self.Z6
+
+        self.felt_slope = (self.A * self.C - 1) / (self.A * self.C + 1)
+        self.Knl = self.C * self.A
+
+        # Init waves
+        self.wg_left = np.zeros(self.wg_length_left)
+        # print('init wg_left shape is: ', self.wg_left.shape)
+        # print(self.wg_left)
+        self.wg_right = np.zeros(self.wg_length_right)
+        # print('init wg_right_shape is: ', self.wg_right.shape)
+        self.string = np.zeros(self.iterations)
+        # print('init string shape is: ', self.string.shape)
+        self.hammer = np.zeros(self.iterations)
+        # print('init hammer shape is ', self.hammer.shape)
+
+        # Init incident waves for strings
+        self.a1 = 0
+        self.a2 = 0
+        self.b3 = -self.a1 - self.a2
+        self.a4 = self.b3
+        self.a5 = self.Z5 * self.hammer_initial_velocity / 2
+        self.b6 = self.a5 - self.gamma_par * (self.a5 - self.a4)
+        self.a7 = self.b6
+        self.b8 = -self.hs_distance / (2 * self.C)
+        self.wave_integrator_delay_old = self.b8 - self.a7
+        self.a8 = nl_felt(self.b8, self.felt_slope)
+        self.wave_integrator_delay = self.a7 - self.a8
+        self.b7 = self.a8 + self.wave_integrator_delay_old
+        self.a6 = self.b7
+        self.b5 = self.a6 - self.gamma_par * (self.a5 - self.a4)
+        self.b4 = -self.a4 + self.a5 + self.b5
+        self.a3 = self.b4
+        self.b1 = self.a1 - self.gamma_ser * (self.a1 + self.a2 + self.a3)
+        self.b2 = -self.a3 - self.b1
+
+        self.hammer_velocity = (self.a5 - self.b5) / self.Z5
+        self.hammer_position = (self.b8 - self.a8) * self.C
+        self.hammer_initial_position = self.hammer_position
+        self.string_velocity = (self.a1 - self.b1) / self.Z1
+        self.string_position = np.double(0)
+
+        # init other values
+        self.string_velocity_old = np.double(0)
+        self.string_position_old = np.double(0)
+        self.hammer_velocity_old = np.double(0)
+        self.hammer_position_old = np.double(0)
+
+        # matrix containing summed waveguide values at each iteration
+        self.string_matrix = np.zeros((self.iterations, self.wg_length // 2))
+        print('Initialized string matrix with shape: ', self.string_matrix.shape)
+        print('total wgLength allocated length is: ', self.wg_length)
+        print("Effective wgLength is: ", self.wg_length // 2)
+
+    def run_simulation(self):
+        """
+        Runs the simulation
+        :return:
+
+        """
+        print('Starting WDF-Piano algorithm')
+        print('Simulation will be run for: ', len(range(0, self.iterations)), ' steps')
+        #  runs the simulation for specified number of iterations
+        for n in range(0, self.iterations):
+            #  Shifts the two waveguides
+            self.a1 = dwg_shift(-self.b1, self.wg_left, n)
+            self.a2 = dwg_shift(self.K * self.b2, self.wg_right, n)
+
+            self.wave_integrator_delay_old = self.wave_integrator_delay
+            self.string_velocity_old = self.string_velocity
+            self.string_position_old = self.string_position
+            self.hammer_velocity_old = self.hammer_velocity
+            self.hammer_position_old = self.hammer_position
+
+            self.b3 = -self.a1 - self.a2
+            self.a4 = self.b3
+            self.a5 = -self.b5
+            self.b6 = self.a5 - self.gamma_par * (self.a5 - self.a4)
+            self.a7 = self.b6
+            self.b8 = self.a7 + self.wave_integrator_delay_old
+            self.a8 = nl_felt(self.b8, self.Knl)
+            self.wave_integrator_delay = self.a7 - self.a8
+            self.b7 = self.a8 + self.wave_integrator_delay_old
+            self.a6 = self.b7
+            self.b5 = self.a6 - self.gamma_par * (self.a5 - self.a4)
+            self.b4 = -self.a4 + self.a5 + self.b5
+            self.a3 = self.b4
+            self.b1 = self.a1 - self.gamma_ser * (self.a1 + self.a2 + self.a3)
+            self.b2 = -self.a3 - self.b1
+
+            self.string_velocity = (self.a1 - self.b1) / self.Z1
+            self.string_position = self.string_position_old + (
+                    self.string_velocity + self.string_velocity_old) * self.Ts / 2
+            self.string[n] = self.string_position
+            self.hammer_velocity = (self.a5 - self.b5) / self.Z5
+            self.hammer_position = self.hammer_position_old + (
+                    self.hammer_velocity + self.hammer_velocity_old) * self.Ts / 2
+            self.hammer[n] = self.hammer_position  # Store hammer position at current iteration
+
+            # Fill string_matrix TODO verify array indexing is correct
+            half_wg_left_length = self.wg_length_left // 2
+            l1 = self.wg_left[0:half_wg_left_length]
+            # print('self.wg_length_left // 2 is: ', self.wg_length_left // 2)
+            # print('l1 size is: ', l1.size)
+            l2 = self.wg_left[half_wg_left_length:self.wg_length_left]
+            # print('l2 size is: ', l2.size)
+            left = np.add(np.flip(l2), l1)
+            # print('left size is: ', left.size)
+            half_wg_right_length = self.wg_length_right // 2
+            r1 = self.wg_right[0:half_wg_right_length]
+            # print('r1 size is: ', r1.size)
+            # print('self.wg_length_right // 2 is: ', self.wg_length_right // 2)
+            r2 = self.wg_right[half_wg_right_length: self.wg_length_right]
+            # print('r2 size is: ', r2.size)
+            right = np.add(np.flip(r2), r1)
+            # print('right size is: ', right.size)
+            new_row = np.concatenate((left, right), axis=0)
+            # self.string_matrix[n, :] = np.concatenate((left, right), axis=0)
+            self.string_matrix[n, :] = new_row
+            # print('new row @ step ', n, ' is: ', new_row)
+            # print('string_matrix[', n, '] is: ', self.string_matrix[n, :])
+            # self.string_matrix[n, 0:left.size] = left
+            # self.string_matrix[n, left.size: self.string_matrix.shape[1]] = right
+            # print('string_matrix[', n, '] size is: ', self.string_matrix[n].size)
+
+        print('Ended WDF-Piano algorithm')
+        file_name = 'python_string_matrix.mat'
+        print('Saving simulation output to: ', file_name)
+        sio.savemat(file_name, {'python_string_matrix': self.string_matrix})
+        return self.string_matrix, self.hammer
+
