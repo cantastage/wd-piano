823ca73c821d82fe2f30c4000aa02c6eb1baf73b
Dario Cantarelli <dario.cantarelli@mail.polimi.it>
2023-05-19 00:59:58 +0200

    Missing changes in prev commit.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PianoKeyboardComponent } from './piano-keyboard/piano-keyboard.component';
 import { ParametersComponent } from './parameters/parameters.component';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { ParametersComponent } from './parameters/parameters.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/parameters/parameters.component.ts
+++ frontend/src/app/parameters/parameters.component.ts
@@ -7,5 +7,32 @@ import { ApiService } from '../api.service';
   styleUrls: ['./parameters.component.scss']
 })
 export class ParametersComponent {
+  minStringFrequency: number = 0;
+  maxStringFrequency: number = 20000;
+  stringFrequency = 440;
+  minStringTension: number = 0; // TODO check suitable range
+  maxStringTension: number = 1000; // TODO check suitable range
+  stringTension: number = 670;
+  minHammerMass: number = 0; // TODO check suitable range
+  maxHammerMass: number = 1000; // TODO check suitable range
+  hammerMass: number = 0;
+  minHammerInitialVelocity: number = 0; // TODO check suitable range
+  maxHammerInitialVelocity: number = 1000; // TODO check suitable range
+  hammerInitialVelocity: number = 0;
+  minHammerStrikePoint: number = 0; // TODO check suitable range
+  maxHammerStrikePoint: number = 1000; // TODO check suitable range
+  hammerStrikePoint: number = 0;
+
+
+  value = 0;
+
+  // DEBUG only TODO remove after testing
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return `${value}`;
+  }
   constructor(private apiService:ApiService) { }
 }
