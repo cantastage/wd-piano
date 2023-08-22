ca78cd9038cf139afa17f4dcfab00a7aa97354de
Dario Cantarelli <dario.cantarelli@mail.polimi.it>
2023-08-22 15:48:21 +0200

    Making piano keyboard responsive.

--- frontend/src/app/piano-keyboard/piano-keyboard.component.ts
+++ frontend/src/app/piano-keyboard/piano-keyboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
 import { PianoKey } from '../model/piano-strings';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-piano-keyboard',
@@ -10,12 +11,12 @@ import { PianoKey } from '../model/piano-strings';
 export class PianoKeyboardComponent {
   // private pianoToMidiOffset = 20; // 20 is the offset from piano key to corresponding midi note number
   // private numKeys = 88; // 88 keys on a piano
-  private unwrappedStrings: Array<PianoKey> = [];
+  unwrappedStringKeys: Array<PianoKey> = []; // TODO maybe add getter and make field private
 
   ngOnInit() {
     this.apiService.getStrings().subscribe((data) => {
       // console.log("ARRIVED FROM SERVER: ", data);
-      this.unwrappedStrings = this.parsePianoKeys(data);
+      this.unwrappedStringKeys = this.parsePianoKeys(data);
     });
   }
 
@@ -42,6 +43,20 @@ export class PianoKeyboardComponent {
   //   let midiNote = pianoKeyIndex + this.pianoToMidiOffset;
   //   return midiNote.toString();
   // }
+
+  public getPianoKeyStyle(pianoKeyIndex: number): string {
+    let noteLabel = this.unwrappedStringKeys[pianoKeyIndex].getNoteLabel();
+    // TODO check if it can be optimized
+    let styleClass = "";
+    console.log("original note label for index " + pianoKeyIndex + ": ", noteLabel);
+    if (noteLabel.includes("d",1)) {
+      styleClass = "black " + noteLabel[0].toLowerCase() + "s"; 
+    } else {
+      styleClass = "white " + noteLabel[0].toLowerCase();
+    }
+    console.log("styleClass: ", styleClass);
+    return styleClass;
+  }
 
   private parsePianoKeys(rawKeys: Array<string>): Array<PianoKey> {
     let pianoKeys: Array<PianoKey> = []; // init array of piano key objects
