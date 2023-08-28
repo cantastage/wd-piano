06899fac90acf036d73a474df695a1be4562707d
Dario Cantarelli <dario.cantarelli@mail.polimi.it>
2023-08-28 21:39:51 +0200

    Added spectral bandwidth backend.

--- frontend/src/app/model/daap-features.ts
+++ frontend/src/app/model/daap-features.ts
@@ -1,5 +1,6 @@
 export interface DAAPFeatures {
     mfccs: string;
     spectralCentroid: string;
+    // spectralBandwidth: string;
     // TODO add all other features when implemented
 }
--- model/daap.py
+++ model/daap.py
@@ -1,8 +1,8 @@
 #  Feature richieste: MFCCs, spectral centroid, spectral roll-off, spectral bandwidth, tonnetz, etc.
-import librosa
 import os
-from matplotlib.figure import Figure
 import numpy as np
+import librosa
+from matplotlib.figure import Figure
 
 from model.settings import Settings
 
@@ -24,26 +24,43 @@ class AudioFeatureExtractor(object):
         :param audio_file_name: the audio file
         :return: the extracted features
         """
+        # init variables
         extracted_features = {}  # init dictionary of extracted features
-        y, sr = librosa.load(os.path.join('media', 'audio', audio_file_name), sr=Settings.get_sampling_freq())  # load audio file
-        fig = Figure()  # init figure container
-        ax = fig.subplots()  # init single plot container
+        audio_file_path = os.path.join('media', 'audio', audio_file_name) # audio file path
+        mfccs_fig = Figure()  # init figure container
+        mfccs_ax = mfccs_fig.subplots()  # init single plot container
+        y, sr = librosa.load(audio_file_path, sr=Settings.get_sampling_freq())  # load audio file
+        S, phase = librosa.magphase(librosa.stft(y=y))  # extract magnitude and phase
+
         # MFCCs
         mfccs = librosa.feature.mfcc(y=y, sr=sr)  # extract mfccs
-        img = librosa.display.specshow(mfccs, x_axis='time', ax=ax)  # create img for mfccs
-        fig.colorbar(img, ax=ax)
-        ax.set(title='MFCCs')
-        extracted_features['mfccs'] = cls.save_feature_plot(fig, 'mfccs', Settings.get_base_filename())  # save mfccs plot
+        mfccs_img = librosa.display.specshow(mfccs, x_axis='time', ax=mfccs_ax)  # create img for mfccs
+        mfccs_fig.colorbar(mfccs_img, ax=mfccs_ax)
+        mfccs_ax.set(title='MFCCs')
+        extracted_features['mfccs'] = cls.save_feature_plot(mfccs_fig, 'mfccs', Settings.get_base_filename())
+
         # Spectral centroid
         # cent = librosa.feature.spectral_centroid(y=y, sr=sr)  # extract spectral centroid
-        S, phase = librosa.magphase(librosa.stft(y=y))  # extract magnitude and phase
-        cent = librosa.feature.spectral_centroid(S=S)
-        times = librosa.times_like(cent)  # extract times
-        librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax)
-        ax.plot(times, cent.T, label='Spectral centroid', color='w')
-        ax.legend(loc='upper right')
-        ax.set(title='log Power spectrogram')
-        extracted_features['spectralCentroid'] = cls.save_feature_plot(fig, 'spectralCentroid', Settings.get_base_filename())
+        centroid_fig = Figure()  # init figure container
+        centroid_ax = centroid_fig.subplots()  # init single plot container
+        centroid = librosa.feature.spectral_centroid(S=S)
+        cent_times = librosa.times_like(centroid)  # extract times
+        librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=centroid_ax)
+        centroid_ax.plot(cent_times, centroid.T, label='Spectral centroid', color='w')
+        centroid_ax.legend(loc='upper right')
+        centroid_ax.set(title='log Power spectrogram')
+        extracted_features['spectralCentroid'] = cls.save_feature_plot(centroid_fig, 'spectralCentroid', Settings.get_base_filename())
+
+        spec_bw_fig = Figure()
+        spec_bw_ax = spec_bw_fig.subplots()
+        spectral_bw = librosa.feature.spectral_bandwidth(S=S)
+        spec_bw_times = librosa.times_like(spectral_bw)
+        librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=spec_bw_ax)
+        spec_bw_ax.fill_between(spec_bw_times, np.maximum(0, centroid[0] - spectral_bw[0]), np.minimum(centroid[0] + spectral_bw[0], sr/2), alpha=0.5, label='Centroid  +- bandwidth')
+        spec_bw_ax.plot(spec_bw_times, centroid[0], label='Spectral centroid', color='w')
+        spec_bw_ax.legend(loc='lower right')
+        extracted_features['spectralBandwidth'] = cls.save_feature_plot(spec_bw_fig, 'spectralBandwidth', Settings.get_base_filename())
+
         return extracted_features
         # fig.savefig(os.path.join('media', 'images', plot_filename), format="png")
 
