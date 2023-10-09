d073c5f350c08897343368f99d52d2dc2640952b
Dario Cantarelli <dario.cantarelli@mail.polimi.it>
2023-10-09 11:54:52 +0200

    Fix comparison visualization.

--- frontend/src/app/compare/compare.component.ts
+++ frontend/src/app/compare/compare.component.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../api.service';
 import { LocalDataService } from '../local-data.service';
 import { DEFAULT_SPECTRAL_ANALYSIS_PARAMETERS, SpectralAnalysisParameters } from '../model/daap-features';
 import { WDResult } from '../model/wd-result';
+import { API_URL } from 'src/env';
 
 @Component({
   selector: 'app-compare',
@@ -24,6 +25,10 @@ export class CompareComponent {
 
   public getCompareFilenames(): Array<string> {
     return this.wdResults.map(wdResult => wdResult.baseFilename);
+  }
+
+  public getPlotUrl(plotName: string): string {
+    return API_URL + '/plot/' + plotName;
   }
 
 
